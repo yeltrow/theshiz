0c0773529108a30b08eedee004d8e0aa88c015fc
wortley <wortley@grey.yeltrowshopllc.com>
2022-03-13 11:13:09 -0400

    Finished initial wire-up of the new mesa board. Mill is functional. Yet to do: Reconcile values for new home switch positions. Update script for fixture G54 offsets now. The position had to change because the home/limit switches have to be avoided now that they are wired in hal as limit switches, too.  This really is better because if the drives go crazy at some point the mill will shut down the control signals to the drives. Changing strategy on offsets to motors for the "ring reading." When I tried to add them with the PID loops offset, they didnt' work. This is probably because of the added delay.  It wasn't really needed anyway.  I can just jog to the 0 markers and then set the G54 position as the oposite of the offsets I want and the fixture position will be correct.  I intend to add a button to go back to the opposite coordinates of the G54 to check the zeros in a convenient way in case I have doubts at some point in a job. I also plan to add a capture of the feedback positions each time there is positive direction velocity on the table and the prox on the handwheel has a positive edge as a live check of following error to try to detect missed steps before things get really bad.

---
+++ nc_files/store_home_adjust_g54.sh
@@ -0,0 +1,6 @@
+axis-remote --mdi "G28.1"
+axis-remote --mdi "(Make the current G28 position a fixed offset from G54)"
+axis-remote --mdi "G10 L20 P1 X[-5.1488] Y[-3.0166] Z[1.8101]"
+axis-remote --mdi "G0 G54 X0 Y0 Z0"
+ 
+
